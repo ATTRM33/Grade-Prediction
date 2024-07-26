e053d23f56ffdf6d19eaa98cd28af350781443e8
ATTRM33 <ATTRM33@gmail.com>
2024-07-25 19:58:43 -0500

    removed debugging code

--- Main_Program.cpp
+++ Main_Program.cpp
@@ -6,14 +6,6 @@
 
 int main() {
 
-    // Test reading from a file
-    std::string inputFilePath = "test.txt";
-    std::vector<std::string> lines = FileHandler::readFromFile(inputFilePath);
-
-    std::cout << "Read lines from " << inputFilePath << ":\n";
-    for (size_t i = 0; i < lines.size(); ++i) {
-        std::cout << lines[i] << "\n";
-    }
 	ConsoleUI ui;
 
 	ui.runProgram();
